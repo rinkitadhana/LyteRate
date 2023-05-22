47039704796ebf57a8f1a3336802a98728f083f0
Rinkit Adhana <therinkit@gmail.com>
2023-05-23 02:58:03 +0530

    fourth commit

--- assets/js/ChatBot-jsfile/responses.js
+++ assets/js/ChatBot-jsfile/responses.js
@@ -13,7 +13,10 @@ function getBotResponse(input) {
         return "Hello sir, welcome to lyterate! How can i help you?";
     } else if (input == "bye") {
         return "Talk to you later!";
-    } else {
+    } else if (input == "tell me about dharampal"){
+        return "Introducing our innovative chatbot, DHARAMPAL! Designed with a primary focus on assisting students, DHARAMPAL is equipped to address a wide range of queries, whether they pertain to coding or website-related concerns. With a deep understanding of various programming languages and web development concepts, DHARAMPAL is your go-to resource for resolving doubts and providing comprehensive solutions.";
+    }
+     else {
         return "Try asking something else!";
     }
 }
